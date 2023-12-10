acc9e0620fb2ccaff7d8a5e57fca18d632f1d792
heraldsandoy <sandoyherald1993@gmail.com>
2023-12-10 23:32:29 +0800

    fix responsive on work section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
 
 function HeroSection() {
   return (
-  <section id="hero" className="flex items-center lg:max-w-[1600px] sm:max-w-[90%] mx-auto py-[60px]">
-    <div className="w-1/2 p-4">
-      <h1 className="font-pexico">I'M HERALD <br />SANDOY</h1>
+    <section id="hero" className="flex items-center 2xl:max-w-[1600px] xl:max-w-[95%] lg:max-w-[95%] md:max-w-[95%] mx-auto py-[60px]">
+      <div className="w-1/2 p-4">
+        <h1 className="font-pexico">
+          I'M HERALD <br />
+          SANDOY
+        </h1>
 
-      <div className="intro-pos mb-10 mt-10">
-        <h2 className="font-proximaBold leading-[40px]">Web Developer</h2>
-        <h3 className="font-proximaSemi">Based on Philippines</h3>
-      </div>
-
-      <Link href="/">
-        <div className="relative bg-blue-bg text-white cta-prop">
-          <div className="skew-top bg-blue-bg"></div>
-            <p className="z-10 font-pexico">CONTACT ME</p>
-          <div className="skew-bottom bg-blue-bg"></div>
+        <div className="intro-pos mb-10 mt-10">
+          <h2 className="font-proximaBold leading-[40px]">Web Developer</h2>
+          <h3 className="font-proximaSemi">Based on Philippines</h3>
         </div>
-      </Link>
-  
-    </div>
-    <div className="w-1/2 p-4 relative min-h-[500px]">
+
+        <Link href="/">
+          <div className="relative bg-blue-bg text-white cta-prop">
+            <div className="skew-top bg-blue-bg"></div>
+            <p className="z-10 font-pexico">CONTACT ME</p>
+            <div className="skew-bottom bg-blue-bg"></div>
+          </div>
+        </Link>
+      </div>
+      <div className="w-1/2 p-4 relative min-h-[500px] hide-on-mobile">
         <Image className="absolute top-0" src="/grey-herald.png" width="1247" height="1193" alt="herald hero image" />
-    </div>
-  </section>
+      </div>
+    </section>
   )
 }
 
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,47 +1,47 @@
 // pages/projects.tsx
-import React from 'react';
-import ProjectSlider from '../components/ProjectSlider';
-import DeckOfCards from '../components/DeckOfCards';
+import React from "react"
+import ProjectSlider from "../components/ProjectSlider"
+import DeckOfCards from "../components/DeckOfCards"
 
 const cardData = [
   {
-    background: '/projects/army.png',
-    title: 'Army Amenities Fund',
-    description: 'AAF Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.',
-    link: 'https://armyamenitiesfund.com.au',
+    background: "/projects/army.png",
+    title: "Army Amenities Fund",
+    description: "AAF Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.",
+    link: "https://armyamenitiesfund.com.au"
   },
   {
-    background: '/projects/jones.png',
-    title: 'Jones Theatrical Group',
-    description: 'JTG Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.',
-    link: 'https://jonestg.co/',
+    background: "/projects/jones.png",
+    title: "Jones Theatrical Group",
+    description: "JTG Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.",
+    link: "https://jonestg.co/"
   },
   {
-    background: '/projects/qfab.png',
-    title: 'Quality Fabrication',
-    description: 'QFAB Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.',
-    link: 'https://www.qualityfabrication.ae/',
+    background: "/projects/qfab.png",
+    title: "Quality Fabrication",
+    description: "QFAB Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.",
+    link: "https://www.qualityfabrication.ae/"
   },
   {
-    background: '/projects/th-b.png',
-    title: 'Total Hearing Care',
-    description: 'THC Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.',
-    link: 'https://totalhearingcare.com/',
+    background: "/projects/th-b.png",
+    title: "Total Hearing Care",
+    description: "THC Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.",
+    link: "https://totalhearingcare.com/"
   },
   {
-    background: '/projects/uniformity.png',
-    title: 'Uniformity',
-    description: 'Uniformity Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.',
-    link: 'https://uniformity.com.au/',
-  },
-];
+    background: "/projects/uniformity.png",
+    title: "Uniformity",
+    description: "Uniformity Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus nisl non arcu accumsan rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula erat quis eros imperdiet, nec lobortis enim viverra.",
+    link: "https://uniformity.com.au/"
+  }
+]
 
 const Projects: React.FC = () => {
   return (
     <section id="projects">
       <div className="relative bg-blue-bg min-h-[900px]">
         <div className="absolute inset-x-0 top-0 bg-blue-bg transform -skew-y-2 origin-top-left h-24 limp"></div>
-        <div className="text-center lg:max-w-[1440px] sm:max-w-[90%] mx-auto pt-11">
+        <div className="text-center 2xl:max-w-[1350px] xl:max-w-[90%] lg:max-w-[90%] md:max-w-[90%] mx-auto pt-11">
           <h2 className="text-white text-[35px] font-pexico">Explore some of my work</h2>
 
           <div className="rotator">
@@ -50,7 +50,7 @@ const Projects: React.FC = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
